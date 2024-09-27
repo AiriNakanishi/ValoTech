f22081037b811e8ba7e4c88310e2f825e20197d5
Airi Nakanishi <b1023165@fun.ac.jp>
2024-09-27 17:40:54 +0900

    splash初期化まで消えないようにした！！

--- valotech/lib/main.dart
+++ valotech/lib/main.dart
@@ -9,11 +9,17 @@ import 'package:valotech/view/firstmain/mainscreen.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
 
+import 'package:flutter_native_splash/flutter_native_splash.dart';
+
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
+  FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
+  // 初期化が完了したら、スプラッシュ画面を削除
+  FlutterNativeSplash.remove();
   runApp(const MyApp());
 }
 
